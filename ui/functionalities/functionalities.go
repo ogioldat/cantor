aeba3ad8e91e2d1dd2265bc26d3d4c97c0519f0c
Michał Wieczorek <mwieczorek@sitel.local>
2021-06-25 23:03:51 +0200

    no back any more

--- ui/functionalities/functionalities.go
+++ ui/functionalities/functionalities.go
@@ -1,11 +1,5 @@
 package functionalities
 
-import (
-	"fmt"
-
-	"github.com/manifoldco/promptui"
-)
-
 func DisplayCurrentCurrencies() {
 
 }
@@ -27,16 +21,5 @@ func CurrencyConversion() {
 }
 
 func Settings() {
-	prompt := promptui.Select{
-		Label: "Wybierz opcję: ",
-		Items: []string{"Powrót"},
-	}
-	_, result, err := prompt.Run()
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return
-	}
-	if result == "Powrót" {
-		print("\033[H\033[2J")
-	}
+
 }
