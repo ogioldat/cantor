7a062b81107eb32405f32bc4a8f18639ae091527
Michał Wieczorek <mwieczorek@sitel.local>
2021-06-25 20:07:41 +0200

    ui folder

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/ogioldat/cantor/web"
 	"github.com/ogioldat/cantor/welcomescreen"
 )
 
 func main() {
+	fmt.Println("Start")
 	welcomescreen.IntroScreen()
-
 	web.GetLatestCurrencies()
 }
